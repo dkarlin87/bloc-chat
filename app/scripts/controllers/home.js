c066c247e7564f93948d357688789cf7dab18fdc
dkarlin87 <dankarlin@soolmedia.com>
2017-04-01 13:37:00 -0500

    checkpoint 3

---
+++ app/scripts/controllers/home.js
@@ -0,0 +1,8 @@
+ (function() {
+     function mainCtrl(Room)
+     this.heroTitle = "Chat your Bloc off!";
+ }
+     angular
+         .module('blocChat')
+         .controller('MainCtrl', mainCtrl);
+ })();
---
+++ app/scripts/services/room.js
@@ -0,0 +1,20 @@
+(function() {
+  function Room($firebaseArray) {
+    var ref = firebase.database().ref().child("rooms");
+    var rooms = $firebaseArray(ref);
+
+    return {
+        
+      all: rooms
+        Room.add = function(room) {
+        rooms.$add(room);
+    };
+        
+    };
+  }
+
+  angular
+    .module('blocChat')
+    .factory('Room', ['$firebaseArray', Room]);
+})();
+
